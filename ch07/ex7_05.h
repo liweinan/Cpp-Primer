dfcc3ce0db46c0811490a1836e67cba7cd13bc35
Weinan Li <l.weinan@gmail.com>
2019-04-14 09:13:37 +0800

    update cmake script

--- ch07/ex7_05.h
+++ ch07/ex7_05.h
@@ -16,13 +16,13 @@
 
 #include <string>
 
-class Person 
-{
+class Person {
     std::string name;
     std::string address;
 public:
-    auto get_name() const -> std::string const& { return name; }
-    auto get_addr() const -> std::string const& { return address; }
+    auto get_name() const -> std::string const & { return name; }
+
+    auto get_addr() const -> std::string const & { return address; }
 };
 
 #endif
